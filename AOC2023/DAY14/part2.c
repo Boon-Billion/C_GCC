5565c8c5461407af67eef381982a7c65fe550c0d
Boon <Boon@DESKTOP-1SBMCV1>
2023-12-20 17:12:06 +0800

    modified:   AOC2023/DAY14/part2.c 	new file:   AOC2023/DAY14/part2.exe

--- AOC2023/DAY14/part2.c
+++ AOC2023/DAY14/part2.c
@@ -2,6 +2,10 @@
 #include<string.h>
 #include<stdlib.h>
 
+int spin_cycle(char** input);
+
+
+
 FILE* file = NULL;
 char str_buffer[256] = { 0 };
 int main(){
@@ -15,16 +19,42 @@ int main(){
         input[line_len - 1] = strdup(str_buffer);
         printf("\t%s", str_buffer);
     }
+    input = realloc(input, (line_len + 1) * sizeof(char*));
+    input[line_len] = NULL;
+
+    spin_cycle(input);
+
+    
+
+    return 0;
+}
+
+int spin_cycle(char** input){
     size_t str_len = strlen(input[0]) - 1;
+    size_t line_len = 0;
+    while(input[++line_len]);
+    
+    int** new_input = malloc(line_len + 1 * sizeof(char*));
+    new_input[line_len] = NULL;
     int* weight = malloc(str_len * sizeof(int));
     int* tag = malloc(str_len * sizeof(int));
     memset(weight, 0, str_len * sizeof(int));
     for(int i = 0; i < str_len; i++){
         tag[i] = line_len;
     }
+    /*
+        N:  i = 0; i < line_len
+            j = 0; j < str_len
+        
+        S:  i = line_len - 1; i <= 0
+            j = 0; j < str_len
+        
+        W:  j = 0; j < str_len
+            i = 0; i < line_len
 
-    int starting_weight = line_len;
-
+        E:  j = str_len - 1; j <= 0;
+            i = 0; i < line_len
+    */
     for(int i = 0; i < line_len; i++){
         for(int j = 0; j < str_len; j++){
             char c_input = input[i][j];
@@ -44,6 +74,4 @@ int main(){
     }
 
     printf("\n\n\tPart1: %d\n\n", sum);
-
-    return 0;
 }
