9a648ced6f5359eda405cc87e29bf5ebb350f06d
Boon <Boon@DESKTOP-1SBMCV1>
2024-01-05 17:31:20 +0800

    modified:   .vscode/test.c 	modified:   .vscode/test.exe

--- .vscode/test.c
+++ .vscode/test.c
@@ -45,6 +45,56 @@ void max_heap_add(max_heap_t heap, void* data, long long priority){
     
 }
 
+void* max_heap_pop(max_heap_t heap){
+    if(heap->size == 0){
+        return 0;
+    }
+    void* temp = heap->node[0].data;
+    if(heap->size == 1){
+        heap->size--;
+        heap->node[0].data = 0;
+        return temp;
+    }
+    heap->size--;
+    heap->node[0].data = heap->node[heap->size].data;
+    heap->node[0].priority = heap->node[heap->size].priority;
+    heap->node[heap->size].data = 0;
+
+    long long n = 0;
+    while (n <= heap->size){
+        if(heap->node[n].priority >= heap->node[(n*2)+1].priority && heap->node[n].priority >= heap->node[(n*2)+2].priority){
+            break;
+        }
+        else if(heap->node[n].priority < heap->node[(n*2)+1].priority){
+            long long temp = heap->node[n].priority;
+            void* data_temp = heap->node[n].data;
+
+            heap->node[n].priority = heap->node[(n*2)+1].priority;
+            heap->node[n].data = heap->node[(n*2)+1].data;
+            heap->node[(n*2)+1].priority = temp;
+            heap->node[(n*2)+1].data = data_temp;
+            n = (n * 2) + 1;
+        }
+        else if (heap->node[n].priority < heap->node[(n*2)+2].priority){
+            if((n*2)+2 > heap->size){
+                break;
+            }
+            long long temp = heap->node[n].priority;
+            void* data_temp = heap->node[n].data;
+
+            heap->node[n].priority = heap->node[(n*2)+2].priority;
+            heap->node[n].data = heap->node[(n*2)+2].data;
+            heap->node[(n*2)+2].priority = temp;
+            heap->node[(n*2)+2].data = data_temp;
+            n = (n * 2) + 2;
+        }
+        
+    }
+    return temp;
+
+    
+}
+
 int main(){
     max_heap_t test = max_heap_new();
     max_heap_add(test, "test35", 35);
@@ -58,6 +108,15 @@ int main(){
     max_heap_add(test, "test26", 26);
     max_heap_add(test, "test31", 31);
 
-    
+    printf("%s\n", max_heap_pop(test));
+    printf("%s\n", max_heap_pop(test));
+    printf("%s\n", max_heap_pop(test));
+    printf("%s\n", max_heap_pop(test));
+    printf("%s\n", max_heap_pop(test));
+    printf("%s\n", max_heap_pop(test));
+    printf("%s\n", max_heap_pop(test));
+    printf("%s\n", max_heap_pop(test));
+    printf("%s\n", max_heap_pop(test));
+    printf("%s\n", max_heap_pop(test));
     return 0;
 }
